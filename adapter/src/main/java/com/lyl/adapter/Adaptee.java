0cafcc5313830919bf9ea1324b04a8b2fc154726
liyl <623285075@qq.com>
2020-04-16 10:47:25 +0800

    add adapter and proxy

---
+++ adapter/src/main/java/com/lyl/adapter/Adaptee.java
@@ -0,0 +1,14 @@
+package com.lyl.adapter;
+
+/**
+ * Adaptee
+ *
+ * @author liyl
+ * @date 2020-4-14
+ */
+public class Adaptee {
+
+    public void request() {
+        System.out.println("上网");
+    }
+}
